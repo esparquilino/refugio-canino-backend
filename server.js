f54cab86170113ee31b6f3770e6b383e22c31654
Angel C <esparquilin2@gmail.com>
2023-05-11 21:52:04 -0600

    third commit

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ mongoose
     console.log(err.message);
   });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
